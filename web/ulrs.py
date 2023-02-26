c67f7f290830a2de70a19ed2e380464340b069c0
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2023-02-26 12:00:10 +0330

    alomost address added

---
+++ web/ulrs.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import *
+
+app_name = 'web'
+urlpatterns = [
+    path('', Home, name='home'),
+
+    path('register/',Userregister,name='register'),
+    path('verify/',UserVerify,name='userverify'),
+    path('forgetpass/',UserForgetPass,name='forgetpass'),
+
+    # path('webpaycontrol/',Webpaycontrol,name='webpaycontrol'),
+    # path('payment/',Payment,name='payment'),
+    path('dashbord/',Dashbord,name='dashbord'),
+    # path('Info/',Info,name='info'),
+
+    path('logout/',UserLogout,name='logout'),
+    path('login/',UserLogin,name='login'),
+
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+
+]
