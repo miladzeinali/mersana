fd99f054b0ad44fcdcdc5430cae7422859073612
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2023-03-04 14:36:34 +0330

    urls modified

--- Product/urls.py
+++ Product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import Products
+
 app_name = 'product'
 urlpatterns = [
     path('/products/',Products,name=products)
+    path('/detailproduct/<int:id>/',ProductDetail,name=products)
 ]
