56bfd06867e552885a12619c8a8f7d33f90a4c32
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2023-03-06 09:10:08 +0330

    headers completed

--- web/ulrs.py
+++ web/ulrs.py
@@ -6,18 +6,10 @@ app_name = 'web'
 urlpatterns = [
     path('', Home, name='home'),
     # path('products/',Shop,name="products"),
-    path('register/',Userregister,name='register'),
-    path('verify/',UserVerify,name='userverify'),
-    path('forgetpass/',UserForgetPass,name='forgetpass'),
-
     # path('webpaycontrol/',Webpaycontrol,name='webpaycontrol'),
     # path('payment/',Payment,name='payment'),
     path('dashbord/',Dashbord,name='dashbord'),
     # path('Info/',Info,name='info'),
-
-    path('logout/',UserLogout,name='logout'),
-    path('login/',UserLogin,name='login'),
-
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
 
