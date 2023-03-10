d5faa19cb842c2456d3b16afda7852f2a8dbea9d
milad zeinali <58866441+miladzeinali@users.noreply.github.com>
2023-03-10 10:07:32 +0330

    sliders modified

--- Product/views.py
+++ Product/views.py
@@ -30,6 +30,33 @@ def Products(request):
     return render(request,'products.html',{'products':products,'orderitems':orderitems,'countfave':countFave,'countitems':countitems,
                                        'total':total})
 
+def SaleProducts(request):
+    products = Product.objects.filter(Sale = True)
+    orderitems = []
+    countitems = []
+    favorits = []
+    total = 0
+    countFave = []
+    user = request.user
+    countitems = len(orderitems)
+    countFave = len(favorits)
+    if user.is_authenticated:
+        try:
+            favorits = Favorits.objects.filter(user=user)
+            countFave = len(favorits)
+            try:
+                order = Order.objects.get(user=user,status='Wpay')
+                orderitems = OrderItem.objects.filter(order=order)
+            except:
+                pass
+            countitems = len(orderitems)
+            for item in orderitems:
+                total += item.quantity*item.price
+        except:
+            pass
+    return render(request,'products.html',{'products':products,'orderitems':orderitems,'countfave':countFave,'countitems':countitems,
+                                       'total':total})
+
 def ProductDetail(request,id):
     try:
         product = Product.objects.get(id=id)
