d2b77f7bbcaca524062cb51f0993aa1210bc2175
miladzeinali <58866441+miladzeinali@users.noreply.github.com>
2023-03-12 09:00:49 +0330

    added url in setting

--- mersana/settings.py
+++ mersana/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gj%&szpw(2)+%^4889%@)72m#@z+8s6%)_p3)4*6_ih&g1m_$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.mersana-co.com','mersana-co.com']
 
 
 # Application definition
